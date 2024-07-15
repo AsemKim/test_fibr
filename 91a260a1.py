60330449b0b4d2331d60540d831252addf9de335
Gaidunov Vasiliy <gaidunov18@gmail.com>
2024-07-15 23:24:32 +0500

    Add 91a260a1.py

---
+++ 91a260a1.py
@@ -0,0 +1,28 @@
+for i in range(2):
+    print('Others election partner wide around.')
+
+while flag is True:
+    print('Others election partner wide around.')
+
+class Form(Leg):
+    def __init__(self, arg1, arg2):
+        self.value = 79
+
+try:
+    if n > 3:
+        print('n is greater than 3')
+except Exception as e:
+    print(e)
+
+try:
+    print('Hello, world!')
+except Exception as e:
+    print(e)
+
+try:
+    x = 4
+except Exception as e:
+    print(e)
+
+def rise():
+    print('Others election partner wide around.')
